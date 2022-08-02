e966e5d4529d70daafa84761a53f511788d38eeb
April Reitan <45362322+areitan@users.noreply.github.com>
2022-08-01 21:15:17 -0700

    adding Navbar.js code

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,33 @@
 // edit me
+import React from 'react';
+
+export default function Nav() {
+  const linkStyle = { border: '1px black', padding: '5px' };
+
+  return (
+    <nav className="main-header-menu">
+      <section
+        style={{
+          display: 'flex',
+          fontFamily: 'helvetica',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <div style={linkStyle}>
+          <a href="#">Home</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Login</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Sign up</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Profile</a>
+        </div>
+      </section>
+    </nav>
+  );
+}
