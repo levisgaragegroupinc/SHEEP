006beb2dee34cbfc2ae4718a0c3751e1672d1df4
April Reitan <45362322+areitan@users.noreply.github.com>
2022-08-01 21:19:50 -0700

    changing Navbar.js code

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,33 +1,58 @@
-// edit me
-import React from 'react';
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
 
-export default function Nav() {
-  const linkStyle = { border: '1px black', padding: '5px' };
+function Nav() {
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">
+              Order History
+            </Link>
+          </li>
+          <li className="mx-1">
+            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">
+              Signup
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  }
 
   return (
-    <nav className="main-header-menu">
-      <section
-        style={{
-          display: 'flex',
-          fontFamily: 'helvetica',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <div style={linkStyle}>
-          <a href="#">Home</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Login</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Sign up</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="#">Profile</a>
-        </div>
-      </section>
-    </nav>
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">
+          <span role="img" aria-label="shopping bag">🛍️</span>
+          -Shop-Shop
+        </Link>
+      </h1>
+
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
   );
 }
+
+export default Nav;
