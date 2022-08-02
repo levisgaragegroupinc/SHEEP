7902f560d801272d100ff3c4ad6892e234cef151
Natalie Bulger <bulgern20@gmail.com>
2022-08-01 20:58:21 -0700

    rough start on resolvers, mutations, and queries, obviously will change when models are created

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,36 @@
-// edit me
+import { gql } from "@apollo/client";
+
+export const QUERY_SINGLE_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const QUERY_ALL_FUNDS = gql`
+  {
+    funds {
+      _id
+      name
+      description
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_FUND = gql``;
+
+export const QUERY_DONATION = gql``;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
