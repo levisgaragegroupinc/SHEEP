a7bc10c8539b7760e3eb1f312af07590c753f31e
Isaac Luton <isaacdluton@gmail.com>
2022-08-02 21:35:21 -0700

    navbar inital formatting.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,53 @@
-import logo from "../images/Sheep-logos.jpeg"
+import logo from "../images/Sheep-logos.jpeg";
 
 const Navbar = () => {
+  //styles
+  const styles = {
+    imgStyle: {
+      width: "8rem",
+      height: "8rem",
+      borderRadius: "30px",
+    },
+    mainContainerStyle: {
+      height: "10vh",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    titleContainerStyle: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    buttonContainerStyle: {
+      height: "100%",
+      display: "flex",
+      justifyContent: "space-around",
+      alignItems: "flex-end",
+    },
+  };
   return (
-    <div>
-        <div>
+    <div style={styles.mainContainerStyle}>
+      <div>
         <img
-        src={logo}
-        alt="Virus Cell Molecule with SHEEP to the right"
-      ></img>
-        </div>
-        <div> 
-            <h1>SHEEP</h1>
-            <h3>Support Helping End and Eradicate Pandemics</h3>
-        </div>
-        <div>
-            <button>Login</button>
-            <button>Signup</button>
-            <button>Myprofile</button>
-        </div>
-      
+          style={styles.imgStyle}
+          src={logo}
+          alt="Virus Cell Molecule with SHEEP to the right"
+        ></img>
+      </div>
+      <div style={styles.titleContainerStyle}>
+        <h1>SHEEP</h1>
+        <h3>Support Helping End and Eradicate Pandemics</h3>
+      </div>
+      <div style={styles.buttonContainerStyle}>
+        <button>Login</button>
+        <button>Signup</button>
+        <button>Myprofile</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
