aaf2837875418d64766b6ceefbec838c852c96b6
Natalie Bulger <bulgern20@gmail.com>
2022-08-03 19:56:24 -0700

    Updating resolvers, mutations, and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,16 +5,45 @@ export const QUERY_SINGLE_USER = gql`
     user {
       firstName
       lastName
+      dollarsDonated
+      projectsFunded
+      orders {
+        _id
+        purchaseDate
+        products {
+          price
+        }
+      }
     }
   }
 `;
 
-export const QUERY_ALL_FUNDS = gql`
-  {
-    funds {
+export const QUERY_PROJECTS = gql`
+  query getProjects($category: ID) {
+    projects(category: $category) {
       _id
       name
       description
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_PROJECTS = gql`
+  {
+    projects {
+      _id
+      name
+      description
+      image
+      products {
+        _id
+        name
+        price
+      }
       category {
         name
       }
@@ -22,15 +51,69 @@ export const QUERY_ALL_FUNDS = gql`
   }
 `;
 
-export const QUERY_SINGLE_FUND = gql``;
+export const QUERY_SINGLE_PROJECT = gql`
+  query getSingleProject($projectId: ID!) {
+    project(projectId: $projectId) {
+      _id
+      name
+      description
+      image
+      products {
+        _id
+        name
+        price
+      }
+      category {
+        name
+      }
+    }
+  }
+`;
 
-export const QUERY_DONATION = gql``;
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    product {
+      _id
+      name
+      price
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PRODUCT = gql`
+  query getSingleProduct($productId: ID!) {
+    product(productId: $productId) {
+      _id
+      name
+      price
+    }
+  }
+`;
 
 export const QUERY_CATEGORIES = gql`
   {
     categories {
       _id
       name
+      projects {
+        _id
+        name
+        description
+        image
+        products {
+          _id
+          name
+          price
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($product: ID!) {
+    checkout(product: $product) {
+      session
     }
   }
 `;
