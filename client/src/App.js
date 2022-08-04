52e2e6b3571ce5eebf391e5cc3ef869b91047c15
Isaac Luton <isaacdluton@gmail.com>
2022-08-03 18:31:31 -0700

    rough outline for home page and link to donation page.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProfilePage from "./pages/ProfilePage";
 import Homepage from "./pages/Homepage";
+import Donate from "./pages/Donate";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/donate" element={<Donate/>} />
         </Routes>
       </div>
     </Router>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,16 +3,23 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const styles = {
-    imgStyle: {
-      width: "7rem",
-      height: "7rem",
-      borderRadius: "50px",
-    },
     mainContainerStyle: {
       height: "15vh",
       display: "flex",
       justifyContent: "space-between",
       alignItems: "center",
+    },
+    imgContainerStyle: {
+      height: "100%",
+      width: "33%",
+      display: "flex",
+      alignItems: "center"
+    },
+    imgStyle: {
+      width: "7rem",
+      height: "7rem",
+      borderRadius: "10px",
+      margin: ".5rem"
     },
     titleContainerStyle: {
       height: "100%",
@@ -20,19 +27,25 @@ const Navbar = () => {
       flexDirection: "column",
       justifyContent: "center",
       alignItems: "center",
+      width:"33%"
     },
-    buttonContainerStyle: {
+    linksContainerStyle: {
       height: "100%",
       display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-around",
-      alignItems: "center",
-      marginRight: ".5rem"
+      justifyContent: "flex-end",
+      alignItems: "flex-end",
+      width: "33%"
     },
+    linksStyle: {
+      margin: ".5rem",
+    },
+    loginStyle: {
+        margin: ".5rem",
+    }
   };
   return (
     <div style={styles.mainContainerStyle}>
-      <div>
+      <div style={styles.imgContainerStyle}>
         <img
           style={styles.imgStyle}
           src={logo}
@@ -43,11 +56,11 @@ const Navbar = () => {
         <h1>SHEEP</h1>
         <h3>Support Helping End and Eradicate Pandemics</h3>
       </div>
-      <div style={styles.buttonContainerStyle}>
-        <h3>Login</h3>
-        <h3>Signup</h3>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/profile"}>Profile</Link>
+      <div style={styles.linksContainerStyle}>
+        <h3 style={styles.loginStyle}>Login</h3>
+        <h3 style={styles.loginStyle}>Signup</h3>
+        <Link to={"/"} style={styles.linksStyle}>Home</Link>
+        <Link to={"/profile"} style={styles.linksStyle}>Profile</Link>
       </div>
     </div>
   );
