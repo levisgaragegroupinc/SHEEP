efc93ce5b9238bc3276a534fd07da90d99b725bb
Travis Bates <tbateswsu@gmail.com>
2022-08-06 11:17:14 -0700

    merging conflicts

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -51,6 +51,7 @@ const typeDefs = gql`
     categories(project: [ID], name: String): [Category]
     category(_id: ID!, project: [ID]): [Category]
     user: User
+    users: [User]
     orders: Order
     order(_id: ID!): Order
     products: Product
