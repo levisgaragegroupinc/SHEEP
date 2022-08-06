a6d3ed932c169f5999ce618464ce0e50d1631707
April Reitan <45362322+areitan@users.noreply.github.com>
2022-08-06 11:16:34 -0700

    adding .env password for stripe and publishable key in Cart component index.js

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -9,7 +9,7 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_51LTrSVC8TMpvZpf0sedUXRS4PClo9Ep07xAHUs9jOdhaFLtPAudN632XcO7dTV07DEItOh8uzuYsCq5BSkFJ9h6D00VYZLSbfZ');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
