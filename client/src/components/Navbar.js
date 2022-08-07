1f0c9a49283094d9e6908a63f2e079d6a0054cf1
Isaac Luton <isaacdluton@gmail.com>
2022-08-07 12:50:16 -0700

    cleaning up navbar and consolidading code.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,61 @@
 import logo from "../images/STOP-logos.png";
 import { Link } from "react-router-dom";
-import  Nav  from "./Nav/index.js"
+import Auth from "../utils/auth";
 
 const Navbar = () => {
+  function showProfile() {
+    if (Auth.loggedIn()) {
+      return (
+        <Link to={"/profile"} style={styles.linksStyle}>
+          Profile
+        </Link>
+      );
+    }
+  }
+  function showHome() {
+    if (window.location.href  !== "/") {
+      return (
+        <Link to={"/"} style={styles.linksStyle}>
+          Home
+        </Link>
+      );
+    }
+  }
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+          <Link to="/" style={styles.linksStyle} onClick={() => Auth.logout()}>
+            Logout
+          </Link>
+      );
+    } else {
+      return ( 
+        <div style={styles.linksStyle}>
+          <Link style={styles.linksStyle} to="/login">Login</Link>
+          <Link style={styles.linksStyle}to="/signup">Signup</Link> 
+          </div>
+      ) 
+    }
+  }
   const styles = {
     mainContainerStyle: {
       height: "15vh",
       display: "flex",
       justifyContent: "space-between",
       alignItems: "center",
-      borderBottom: ".2rem solid black"
+      borderBottom: ".2rem solid black",
     },
     imgContainerStyle: {
       height: "100%",
       width: "33%",
       display: "flex",
-      alignItems: "center"
+      alignItems: "center",
     },
     imgStyle: {
       width: "10rem",
       height: "5rem",
       borderRadius: "10px",
-      margin: ".5rem"
+      margin: ".5rem",
     },
     titleContainerStyle: {
       height: "100%",
@@ -29,21 +63,21 @@ const Navbar = () => {
       flexDirection: "column",
       justifyContent: "center",
       alignItems: "center",
-      width:"33%"
+      width: "33%",
     },
     linksContainerStyle: {
       height: "100%",
       display: "flex",
       justifyContent: "flex-end",
       alignItems: "flex-end",
-      width: "33%"
+      width: "33%",
     },
     linksStyle: {
       margin: ".5rem",
     },
     loginStyle: {
-        margin: ".5rem",
-    }
+      margin: ".5rem",
+    },
   };
   return (
     <div style={styles.mainContainerStyle}>
@@ -59,9 +93,9 @@ const Navbar = () => {
         <h3>Support The Obliteration of Pandemics</h3>
       </div>
       <div style={styles.linksContainerStyle}>
-        <Link to={"/"} style={styles.linksStyle}>Home</Link>
-        <Link to={"/profile"} style={styles.linksStyle}>Profile</Link>
-        < Nav />
+        {showHome()}
+        {showProfile()}
+        {showNavigation()}
       </div>
       {/* temp sucesspage move to swipe success return link */}
       <Link to={"/success"}>Temp SuccessPage</Link>
