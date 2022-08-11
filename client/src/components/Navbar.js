3bfefbd06ee17f991b613652a5ac771535d9012a
Isaac Luton <isaacdluton@gmail.com>
2022-08-10 22:52:06 -0700

    styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 import "../styles/links.css";
 import "../styles/headers.css";
+import virusOne from "../assets/virusOne.png"
+import virusTwo from "../assets/virusTwo.png"
+import virusThree from "../assets/virusThree.png"
+import virusFour from "../assets/virusFour.png"
 
 const Navbar = () => {
   function showProfile() {
@@ -96,6 +100,10 @@ const Navbar = () => {
           src={logo}
           alt="Virus Cell Molecule with STOP to the right"
         ></img>
+        <img src={virusOne} className="navVirus"></img>
+        <img src={virusTwo}></img>
+        <img src={virusThree}></img>
+        <img src={virusFour}></img>
       </div>
       <div className= "navlinkMobile" style={styles.titleContainerStyle}>
         <h1 className="titleMobile">S.T.O.P.</h1>
